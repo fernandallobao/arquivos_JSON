0d60a564b9010b2cb62afa82278be04ce0e957ae
fernandallobao <fernandafcclobao@gmail.com>
2024-09-25 15:38:58 -0300

    Atualizando o projeto

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
         print('1 - Criar novo arquirvo JSON')
         print('2 - Abrir e ler arquirvo JSON')
         print('3 - Salvar novo usuário')
+        print('4 - Alterar dados do usuário')
+        print('5 - Deletar usuário')
         print('0 - Sair do programa.')
 
         op = input('Informe a opção desejada: ')
@@ -42,16 +44,60 @@ if __name__ == '__main__':
                     continue
             case '3':
                 try:
-                    usuario = {}
-                    campos = ('nome', 'cpf', 'email', 'profissao')
+                    #primeira forma de fazer
+                    # usuario = {}
+                    # campos = ('nome', 'cpf', 'email', 'profissao')
+                    # print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
+                    # usuario['codigo'] = len(usuarios) #verfifica a quantidade de usuários e gera um novo código para o novo usuario
+                    # for campo in campos:
+                    #     usuario[campo] = input(f'Informe o campo {campo.capitalize()}:')
+                    # usuarios.append(usuario)
+                    # print(m.salvar_dados(usuarios, abrir_arquivo))
+                    #--------------------------------------------
+                    #segunda forma de fazer
                     print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
-                    usuario['codigo'] = len(usuarios) #verfifica a quantidade de usuários e gera um novo código para o novo usuario
-                    for campo in campos:
-                        usuario[campo] = input(f'Informe o campo {campo.capitalize()}:')
+                    p.codigo = len(usuarios)
+                    p.nome = input('Informe o nome: ')
+                    p.cpf = input('Informe o cpf: ')
+                    p.email = input('Informe o email: ')
+                    p.profissao = input('Informe a profissao: ')
+                    usuario = dict(codigo=p.codigo, nome=p.nome, cpf=p.cpf, email=p.email, profissao=p.profissao) #dict outra forma de criar um dicionário
                     usuarios.append(usuario)
                     print(m.salvar_dados(usuarios, abrir_arquivo))
                 except Exception as e:
                     print(f'Não foi possível realizar a operação.{e}')
+                finally:
+                    continue
+            case '4':
+                try:
+                    print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
+                    codigo = int(input('Informe o código do usuário que deseja alterar: '))
+                    for campo in usuarios[codigo]:
+                        print(f'Valor do campo código: {campo}:{usuarios[codigo].get(campo)}')
+                        novo_dado = input(f'Informe o novo dado do campo {campo} ou aperte "Enter" caso deseje manter o mesmo valor: ')
+                        if novo_dado:
+                            usuarios[codigo][campo] = novo_dado #no dict usuarios, ele pega o valor da variavel codigo e subistitui o valor no campo e novo_dado recebe esse novo valor
+                        else:
+                            ...
+                    print(m.salvar_dados(usuarios, abrir_arquivo)) #faz a gravação do JSON
+                except Exception as e:
+                    print(f'Não foi possivel alterar os dados!.{e}')
+                finally:
+                    continue
+            case '5':
+                try:
+                    print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
+                    codigo = int(input('Informe o código do usuário que deseja deletar: '))
+                    nome_deletado = usuarios[codigo]['nome']
+                    confirmacao = input(f'Deseja dele o usuário {nome_deletado}? Digite "SIM" para confirmar: ').upper()
+                    if confirmacao == 'SIM':
+                        del(usuarios[codigo])
+                        print(m.salvar_dados(usuarios, abrir_arquivo))
+                        print(f'Usuário {nome_deletado} deletado com sucesso!')
+                    else:
+                        print(f'Usuário {nome_deletado} não foi excluído!')
+                except Exception as e:
+                    print(f'Não foi possivel alterar os dados!.{e}')
                 finally:
                     continue
             case _:
